e045dafd9064721b33d0564e42fc788e193e88dc
Guilherme Mariano Jovanelli <guilhermemjovanelli@gmail.com>
2025-11-04 11:34:03 -0300

    Encrementos no 'arquivo server.js'

--- Aula 7 - Node.js/Node JS e API/server.js
+++ Aula 7 - Node.js/Node JS e API/server.js
@@ -6,6 +6,19 @@ app.get('/', (req,res) => {
     res.send("Hello World!");
 });
 
+// Rota 2
+app.get('/sobre', (req,res) => {
+    res.send('Esta é a rota sobre o sistema!');
+});
+
+// Rota 3
+app.get('/usuarios', (req,res) => {
+    res.json([
+        {id: 1, nome: 'Giovanna'},
+        {id: 2, nome: 'Guilherme'}
+    ])
+});
+
 // Iniciar o servidor
 app.listen(3000,() => {
     console.log('Servidor rodando em http://localhost:3000');
