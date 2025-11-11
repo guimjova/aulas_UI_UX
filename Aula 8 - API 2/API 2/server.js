362bdc76a36f2cef68e854077f9b5f25f7c8e518
Guilherme Mariano Jovanelli <guilhermemjovanelli@gmail.com>
2025-11-11 11:38:46 -0300

    Criação do Banco de Dados e 'server.js'

---
+++ Aula 8 - API 2/API 2/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
+const app = express();
+const PORT = 3000;
+
+// Middleware para JSON
+app.use(express.json());
+
+// Criar ou abrir o banco
+const db = new sqlite3.Database("./database.db");
+
+// Rota de inserção
+app.post('/notas', (req,res) => {
+    const {titulo, conteudo} = req.body;
+    db.run(
+        "INSERT INTO notas (titulos , conteudo) VALUES (?, ?)",
+        [titulo, conteudo],
+        function (err){
+            if (err) return res.status(500).json({error: err.message});
+            res.json({id:this.lastID, titulo, conteudo})
+        }
+    );
+});
+
+// Cria a tabela (se não existir)
+db.run(`CREATE TABLE IF NOR EXISTS notas(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    titulo TEXT,
+    conteudo TEXT
+)`);
+
+// Inicia o servidor
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
